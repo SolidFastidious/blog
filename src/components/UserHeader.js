7019ecbdabbc1ea9ad81861bb0af0bbb37fcb041
SolidFastidious <davidhuang6492@gmail.com>
2020-01-06 02:49:41 -0500

    finalized with the app blog

--- src/components/UserHeader.js
+++ src/components/UserHeader.js
@@ -1,11 +1,7 @@
 import React from "react";
 import { connect } from "react-redux";
-import { fetchUser } from "../actions";
 
 class UserHeader extends React.Component {
-  componentDidMount() {
-    this.props.fetchUser(this.props.userId);
-  }
   render() {
     const { user } = this.props;
     if (!user) {
@@ -16,7 +12,7 @@ class UserHeader extends React.Component {
 }
 
 const mapStateToProps = (state, ownProps) => {
-  return { users: state.users.find(user => user.id === ownProps.userId) };
+  return { user: state.users.find(user => user.id === ownProps.userId) };
 };
 
-export default connect(mapStateToProps, { fetchUser })(UserHeader);
+export default connect(mapStateToProps)(UserHeader);

87856165445dc45a436bdbdba40fee01ecf55d53
SolidFastidious <davidhuang6492@gmail.com>
2020-01-06 01:38:26 -0500

    add memoize

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,5 +1,7 @@
 import { combineReducers } from "redux";
 import postsReducer from "./postsReducer";
+import usersReducer from "./userReducer";
 export default combineReducers({
-  posts: postsReducer
+  posts: postsReducer,
+  users: usersReducer
 });
